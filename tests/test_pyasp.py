c0bb3f60c5850fcdf3eb6575b48e81e8dcf5a1f7
Carlos Afonso <cafonso@e-valua.es>
2014-10-27 20:29:04 +0100

    Tests were unbelievably broken, yet passing.

--- tests/test_pyasp.py
+++ tests/test_pyasp.py
@@ -6,12 +6,15 @@ from pyasp.pyasp import Pyasp
 
 class TestPyasp(unittest.TestCase):
 	def setup_method(self, method):
+		self.pyasp = Pyasp()
+
+	def test_default_cookie_jar(self):
 		"""Does the library use the default cookie
 		jar file if none is specified?"""
 		self.pyasp = Pyasp()
 		assert self.pyasp.cookiejar == Pyasp.DEFAULT_COOKIEJAR
 
-	def test_default_cookie_jar(self):
+	def test_custom_cookie_jar(self):
 		"""Does the library use the specified cookie
 		jar file if one is provided?"""
 		self.pyasp = Pyasp(cookiejar="/tmp/custom.jar")
