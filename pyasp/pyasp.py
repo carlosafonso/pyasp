4ecb961a0b13014e5e73efdaafeca8c5d60b2147
Carlos Afonso <cafonso@e-valua.es>
2014-10-30 20:34:57 +0100

    Implemented get() and post()

--- pyasp/pyasp.py
+++ pyasp/pyasp.py
@@ -1,7 +1,14 @@
 import os
+import requests
 
 class Pyasp(object):
-	DEFAULT_COOKIEJAR = "/tmp/pyasp.cookie.jar"
+	DEFAULT_COOKIEJAR = "/tmp/pyasp.cookiejar"
 
 	def __init__(self, cookiejar=DEFAULT_COOKIEJAR):
 		self.cookiejar = cookiejar
+
+	def get(self, url):
+		return requests.get(url)
+
+	def post(self, url):
+		return requests.post(url)
--- tests/test_pyasp.py
+++ tests/test_pyasp.py
@@ -1,3 +1,4 @@
+import mock
 import os
 import pytest
 import unittest
@@ -11,11 +12,27 @@ class TestPyasp(unittest.TestCase):
 	def test_default_cookie_jar(self):
 		"""Does the library use the default cookie
 		jar file if none is specified?"""
-		self.pyasp = Pyasp()
 		assert self.pyasp.cookiejar == Pyasp.DEFAULT_COOKIEJAR
 
 	def test_custom_cookie_jar(self):
 		"""Does the library use the specified cookie
 		jar file if one is provided?"""
-		self.pyasp = Pyasp(cookiejar="/tmp/custom.jar")
-		assert self.pyasp.cookiejar == "/tmp/custom.jar"
+		self.pyasp = Pyasp(cookiejar="/tmp/customjar")
+
+		assert self.pyasp.cookiejar == "/tmp/customjar"
+
+	@mock.patch('pyasp.pyasp.requests')
+	def test_get(self, mock_requests):
+		url = "http://anyurl"
+
+		response = self.pyasp.get(url)
+
+		mock_requests.get.assert_called_with(url)
+
+	@mock.patch('pyasp.pyasp.requests')
+	def test_post(self, mock_requests):
+		url = "http://anyurl"
+
+		response = self.pyasp.post(url)
+		
+		mock_requests.post.assert_called_with(url)
