b662a3162a4a9a5ed4a927f17bc39cbc1356b2db
Carlos Afonso <cafonso@e-valua.es>
2014-10-31 22:18:42 +0100

    Initial parsing of __VIEWSTATE fields.

--- pyasp/pyasp.py
+++ pyasp/pyasp.py
@@ -1,14 +1,33 @@
 import os
 import requests
 
+from bs4 import BeautifulSoup
+
 class Pyasp(object):
 	DEFAULT_COOKIEJAR = "/tmp/pyasp.cookiejar"
 
 	def __init__(self, cookiejar=DEFAULT_COOKIEJAR):
 		self.cookiejar = cookiejar
+		self.viewstates = []
+		self.eventvalidation = None
 
 	def get(self, url):
-		return requests.get(url)
+		return self.__request(method='get', url=url)
 
 	def post(self, url):
-		return requests.post(url)
+		return self.__request(method='post', url=url)
+
+	def __request(self, method, url):
+		methods = {'get': requests.get, 'post': requests.post}
+
+		response = methods[method](url)
+		try:
+			soup = BeautifulSoup(response.text)
+
+			tag = soup.find('input', {'name': '__VIEWSTATE'})
+			if tag is not None:
+				self.viewstates = [tag['value']]
+		except TypeError:
+			pass
+
+		return response
--- tests/test_pyasp.py
+++ tests/test_pyasp.py
@@ -1,13 +1,19 @@
 import mock
 import os
 import pytest
+import responses
 import unittest
 
 from pyasp.pyasp import Pyasp
 
 class TestPyasp(unittest.TestCase):
+	ANY_URL = "http://anyurl"
+	ANY_RESPONSE_BODY = "<html><body></body></html>"
+	SAMPLE_B64 = "ZmluZ2VycyBjcm9zc2VkLCB0aGlzIHRlc3Qgc2hhbGwgbm90IGZhaWwh"
+
 	def setup_method(self, method):
 		self.pyasp = Pyasp()
+		responses.reset()
 
 	def test_default_cookie_jar(self):
 		"""Does the library use the default cookie
@@ -21,18 +27,39 @@ class TestPyasp(unittest.TestCase):
 
 		assert self.pyasp.cookiejar == "/tmp/customjar"
 
+	def test_state_vars_empty_on_init(self):
+		"""Are all state variables correctly initialized?"""
+		assert self.pyasp.viewstates == []
+		assert self.pyasp.eventvalidation == None
+
 	@mock.patch('pyasp.pyasp.requests')
 	def test_get(self, mock_requests):
-		url = "http://anyurl"
+		"""Are GET requests being issued correctly?"""
+		url = TestPyasp.ANY_URL
 
-		response = self.pyasp.get(url)
-
+		self.pyasp.get(url)
+		
 		mock_requests.get.assert_called_with(url)
 
 	@mock.patch('pyasp.pyasp.requests')
 	def test_post(self, mock_requests):
-		url = "http://anyurl"
+		"""Are POST requests being issued correctly?"""
+		url = TestPyasp.ANY_URL
 
-		response = self.pyasp.post(url)
+		self.pyasp.post(url)
 		
 		mock_requests.post.assert_called_with(url)
+
+	@responses.activate
+	def test_single_viewstate_parsing(self):
+		"""Does the library correctly parse a single
+		__VIEWSTATE field?"""
+		responses.add(
+			responses.GET, TestPyasp.ANY_URL,
+			body='<html><body><input type="hidden" name="__VIEWSTATE" value="{}"/></body></html>'.format(TestPyasp.SAMPLE_B64))
+
+		self.pyasp.get(TestPyasp.ANY_URL)
+
+		assert len(self.pyasp.viewstates) == 1
+		assert self.pyasp.viewstates[0] == TestPyasp.SAMPLE_B64
+
